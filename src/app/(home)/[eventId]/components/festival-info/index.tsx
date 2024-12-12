207d84d823983ea742f0ff33dbe2ddf3bc6af657
wafu-alt <rhfemtlfem@naver.com>
2024-12-12 16:33:46 +0900

    Update:[eventId/page] alert에 대한 에러 수정, 로그에 색상 추가

--- src/app/(home)/[eventId]/components/festival-info/index.tsx
+++ src/app/(home)/[eventId]/components/festival-info/index.tsx
@@ -14,7 +14,11 @@ import KaKaoMap from '@/components/kakao-map';
 export default function FestivalInfoPage({ success, message, event }: IFestivalInfoProps) {
   // SSR에서 정보를 불러오기 실패 했을때 경고문과 새로고침 실행
   if (!success) {
-    alert(`${message}에 의해 오류가 있습니다.\n잠시후 다시 접속 부탁드립니다.`);
+    console.log(
+      `%cError%c : ${message}에 의해 오류가 있습니다.\n잠시후 다시 접속 부탁드립니다.`,
+      'background-color: yellow; color: black; font-weight: bold; ',
+      ''
+    );
   }
 
   /** html이 포함한 string이 string 아닐경우 빈 string으로 return하여 에러를 예방한다 */
